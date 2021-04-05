8832e7eff51842b9bd27cdc942160c8d2d7975b2
=danaisboss <yjk9313@gmail.com>
2021-04-05 13:16:18 +0900

    comment 사용자인증(auth-middleware)적용

--- routes/Routes.js
+++ routes/Routes.js
@@ -1,13 +1,24 @@
 const express = require("express");
-const { postUpload, detail, postEdit, deletePost } = require("../controller/postController.js");
-const postRouter = express.Router();
-const multer = require('multer')
+const { postUpload, 
+        detail, 
+        postEdit, 
+        deletePost } = require("../controller/postController.js");
+const { commentUpload, 
+        commentDelete, 
+        commentEdit } = require("../controller/commentController.js");
+const authMiddleware = require("../middlewares/auth-middleware.js");        
+const multer = require('multer');
 const upload = multer({ dest: 'public' })
+const postRouter = express.Router();
+const commentRouter = express.Router();
 
 postRouter.post('/upload', upload.array('file'), postUpload);
 postRouter.post('/detail/:id', detail);
-postRouter.post('/detail/:id/edit', postEdit);
-postRouter.post('/detail/:id/edit', postEdit);
+postRouter.put('/detail/:id/edit', postEdit);
 postRouter.delete('/detail/:id/delete', deletePost);
 
-module.exports = { postRouter };
+commentRouter.post('/:id/comment', authMiddleware, commentUpload);
+commentRouter.put('/:id/comment/edit', commentEdit);
+commentRouter.delete('/:id/comment/delete', commentDelete);
+
+module.exports = { postRouter, commentRouter };
